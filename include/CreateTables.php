89fe4883583c5395da98730daeea0f7ae17351c2
Jacob Bednard <jacob.bednard@gmail.com>
2018-01-13 17:45:46 -0500

    Init push of DAO

---
+++ include/CreateTables.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * User: Jake
+ * Date: 1/13/2018
+ * Time: 3:03 PM
+ */
+
+$servername = "localhost";
+$username = "";
+$password = "";
+
+?>
---
+++ include/DatabaseConnection.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * User: Jake-Desktop
+ * Date: 1/13/2018
+ * Time: 3:06 PM
+ */
+
+class DB {
+
+    private $server;
+    private $database;
+    private $username;
+    private $password;
+
+    function __construct() {
+
+        $ini = parse_ini_file("../configuration.ini");
+
+        $this->server = $ini["db_address"];
+        $this->database = $ini["db_name"];
+        $this->username = $ini["db_username"];
+        $this->password = $ini["db_password"];
+
+    }
+
+    function select($query) {
+
+        $connection = new mysqli($this->server, $this->username, $this->password, $this->database);
+
+        if($connection->connect_error) {
+            die("Connection Failed: " . $connection->connect_error);
+        }
+
+        $result = $connection->query($query);
+
+        $rows = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+
+        return json_encode($rows);
+
+    }
+
+    function query($query) {
+
+        $connection = new mysqli($this->server, $this->username, $this->password, $this->database);
+
+        if($connection->connect_error) {
+            die("Connection Failed: " . $connection->connect_error);
+        }
+
+        $result = $connection->query($query);
+
+        $rows = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+
+        return;
+
+    }
+
+}
